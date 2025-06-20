1d6a7ace67e1d4c4fa12cbbdde645cddfae2cd06
vikaskhoday <60385474+vikaskhoday@users.noreply.github.com>
2025-06-20 20:16:31 +0530

    commit from website

---
+++ V1_EC2.tf
@@ -0,0 +1,11 @@
+provider "aws"  {
+    region = "ap-south-1"
+}
+
+//resource "aws_instance" "demo-server" {
+//    ami = "ami-0f918f7e67a3323f0"
+//    instance_type = "t2.micro"
+//    tags = {
+//      Name = "demo-server"
+//    }
+//}    
