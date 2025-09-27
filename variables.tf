3ab724f7ed298c618f6f66b8ee706eb8874f349b
vikaskhoday <60385474+vikaskhoday@users.noreply.github.com>
2025-09-27 12:21:41 +0530

    Update variables.tf
    Added Variables

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in."
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-1"
 }
 
 variable "aws_access_key" {
