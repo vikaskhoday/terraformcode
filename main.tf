f2fa1142445d19d7eea772c270c74539126984bf
vikaskhoday <60385474+vikaskhoday@users.noreply.github.com>
2025-09-27 15:04:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,3 +39,43 @@ output "secret_access_key" {
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+
+# Data source to retrieve the existing VPC
+data "aws_vpc" "main-vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["main-vpc"] # Replace with the name tag of your existing VPC
+  }
+  # Alternatively, you can use the VPC ID directly if known:
+  # id = "vpc-xxxxxxxxxxxxxxxxx"
+}
+
+# Create a public subnet
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = data.aws_vpc.existing_vpc.id
+  cidr_block              = "10.0.1.0/24" # Replace with your desired CIDR block
+  availability_zone       = "us-west-1a" # Replace with your desired AZ
+  map_public_ip_on_launch = true # Set to true for public subnets
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+# Create a private subnet
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = data.aws_vpc.existing_vpc.id
+  cidr_block        = "10.0.2.0/24" # Replace with your desired CIDR block
+  availability_zone = "us-west-1b" # Replace with your desired AZ
+  # map_public_ip_on_launch is false by default for private subnets
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+# Output the IDs of the created subnets
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet_1.id
+}
